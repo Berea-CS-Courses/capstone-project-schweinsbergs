679ca730408dcc6db1eb79699f890f7e14e85a58
Sam Schweinsberg <schweinsbergs@berea.edu>
2021-05-12 23:01:39 -0400

    Added code for week 6 check in.

--- Code/recipeApp.py
+++ Code/recipeApp.py
@@ -2,8 +2,12 @@ import pandas as pd  # for dataframe
 import pickle  # to save dataframe
 from recipe_scrapers import scrape_me  # recipe scraper
 import openpyxl  # for working with excel files
-from fuzzywuzzy import fuzz
 import re
+import tkinter as tk
+from tkinter import simpledialog
+from pandasgui import show
+import tabloo
+
 
 # Assistance with the pickle code from:
 # https://www.youtube.com/watch?v=WkIW0YLoQEE&list=PLQVvvaa0QuDc-3szzjeP6N6b0aDrrKyL-&t=507s&ab_channel=sentdex
@@ -109,24 +113,25 @@ class testScrape:
         # for ind in self.recipeLoad.index:
         #     print((self.recipeLoad['ingredients'][ind]))
 
-    def user_input(self):
-        """
-        Takes the user input and makes it a list of strings.
-        TODO: Make all of the input lower case
-        :return:
-        """
-
-        userinput = input("Enter your ingredients separated by a space.")
-
-        self.user_ingredients = userinput.split()  # splits each ingredient up
-        # print("ingredients:", self.user_ingredients)
+    # def user_input(self):
+    #     """
+    #     Takes the user input and makes it a list of strings.
+    #     :return:
+    #     """
+    #
+    #     userinput = input("Enter your ingredients separated by a space.")
+    #
+    #     #userinput = self.userentry.get()
+    #
+    #     self.user_ingredients = userinput.split()  # splits each ingredient up
+    #     print("ingredients:", self.user_ingredients)
 
     def compare_ingredients(self):
         """
         Compares the dataframe's ingredients list and the user's.
         This might be helpful: https://towardsdatascience.com/dealing-with-list-values-in-pandas-dataframes-a177e534f173
 
-        https://www.datacamp.com/community/tutorials/fuzzy-string-python
+        https://stackoverflow.com/questions/35240528/reverse-dataframes-rows-order-with-pandas
 
         So much thanks to Mario and Jesse! Emely and Kaleb helped, too. :)
         :return:
@@ -148,43 +153,27 @@ class testScrape:
            #print(self.recipeLoad['Title'][ind],fraction)
         self.recipeLoad['Percentage'] = fractionList
         self.recipeLoad.sort_values(by='Percentage', inplace=True)
-        print(self.recipeLoad['Percentage'])
-
-           # for ingredient in ingredientList: # For each ingredient in the list,
-           #      for word in ingredient.split(): # Split it up into a word
-           #          for userword in self.user_ingredients: # iterate over the user's list
-           #              #TODO:
-           #              if word.lower() == userword.lower():
-           #                  print("Match!")
+        reversed_dataframe = self.recipeLoad.iloc[::-1]
+        #print(self.recipeLoad['Percentage'], self.recipeLoad['Title'])
+        #print(reversed_dataframe['Percentage'], reversed_dataframe['Title'])
+        #tabloo.show(reversed_dataframe)
+        show(reversed_dataframe)
 
 
 
+    def user_input_window(self):
+        """
+        Help from: https://djangocentral.com/creating-user-input-dialog/
+        :return:
+        """
 
+        canvas = tk.Tk()
+        canvas.withdraw()
+        self.entry = simpledialog.askstring(title="Ingredient Input",
+                                  prompt="Please enter your ingredients separated by a space!")
 
-
-
-
-
-
-
-
-           #matches = fuzz.token_set_ratio(ingredientList,self.user_ingredients)
-
-
-
-
-
-
-
-            #match_list = []
-
-            #print(match_tracker, self.user_ingredients)
-            #calc = len(self.recipeLoad['ingredients'][ind])
-            #finishedcalc = (match_tracker / calc) * 100
-            #if finishedcalc > 60:
-                #print(self.recipeLoad['Title'[ind]])
-
-
+        self.user_ingredients = self.entry.split()
+        print(self.user_ingredients)
 
 
 
@@ -197,11 +186,13 @@ def main():
     test = testScrape('scraper', 'recipeInfo', 'df', 'title', 'totaltime', 'yields', 'ingredients', 'recipeLoad',
                       'user_ingredients', 'Instructions')
 
-    # test.dataframe()  uncomment this if you wanna add more info to the dataframe. on god dont do it otherwise.
+    # test.dataframe() uncomment this if you wanna add more info to the dataframe. or ruin my life. on god dont do it otherwise.
     # test.pickle_jar()
     test.open_pickle_jar()
-    test.user_input()
+    test.user_input_window()
+    #test.user_input()
     test.compare_ingredients()
-
+    # app = SeaofBTCapp()
+    # app.mainloop()
 
 main()
